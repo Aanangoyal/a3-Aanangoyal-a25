325684d187ebf80868195c57c3a79e463fa0643c
Aanan Goyal <goyalaanan2004@gmail.com>
2025-09-10 23:31:12 -0400

    some

--- server.js
+++ server.js
@@ -103,6 +103,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/results', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'results.html'));
+});
+
 // Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
